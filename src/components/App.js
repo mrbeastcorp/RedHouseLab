92c7956205908a7f995e28806f2312d9ccbf701f
Bruno <cerutti.bruno@gmail.com>
2017-08-08 23:09:49 +0200

    first article

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Article from './Article';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Article />
       </div> 
     );
   }
